83e27368e06b8d561dad749bdc1172a0409e36f7
Gaurav Sharma <gks281263@gmail.com>
2025-09-13 19:31:34 +0530

    update portfolio content

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Karthik_P/",   // 👈 this must match your repo name
 })
